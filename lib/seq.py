ff9b68af5a2320016668db45ef4f9afef5a211c4
Tim Sackton <tsackton@g.harvard.edu>
2022-11-02 16:22:34 -0400

    update phase parsing for partial neg strand seqs

--- lib/seq.py
+++ lib/seq.py
@@ -137,7 +137,7 @@ def extractCDS(globs):
             sorted_starts = sorted(list(exon_coords.keys()), reverse=True);
             globs['annotation'][transcript]['coding-start'] = int(exon_coords[sorted_starts[0]])
             #the end position of the last exon is the coding start, where last exon = first sorted starts because sorted starts is reversed
-            globs['annotation'][transcript]['start-frame'] = int(exon_phase[int(exon_coords[sorted_starts[0]])])
+            globs['annotation'][transcript]['start-frame'] = int(exon_phase[exon_coords[sorted_starts[0]+1]])
             
         # Make sure the exons are sorted correctly, reversing the order if the strand is "-"
 
